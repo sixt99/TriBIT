535d143d7cfe5833c92ebdfa0308be1a86425770
Sixte Oriol Llenas Segura <sixte99@gmail.com>
2026-07-10 19:34:23 +0200

    to be honest I dont remember what I changed

--- run_benchmarks/run_single_gpu.sh
+++ run_benchmarks/run_single_gpu.sh
@@ -3,17 +3,12 @@
 #SBATCH --qos=acc_bsccs
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=20
-#SBATCH --time 05:00:00
-
-#set -euo pipefail # Exit the bash script if something fails
-#module purge; module load mkl intel impi boost cuda/12.8 python/3.12.1 gcc/13.2.0 sqlite3
-module purge; module load cuda/12.8 intel mkl python/3.12.1 sqlite3
-
-export CUDA_VISIBLE_DEVICES=0
+#SBATCH --time 02:00:00
 
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 echo "Begin: $timestamp"
 
+export CUDA_VISIBLE_DEVICES=0
 python3 run_single_gpu.py \
 	--exe_path ./tribit \
 	--data_path ../data \
