9d9d928e7d9fdff11dd4edfe9de40bea050a8de1
Sixte Oriol Llenas Segura <sixte99@gmail.com>
2026-07-10 14:39:26 +0200

    Fixed two matrices that were not being downloaded

--- download/download_data_multi.sh
+++ download/download_data_multi.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 DATASETS=(
-  "twitter-2010"
-  "gsh-2015"
-  "uk-2006-09"
-  "uk-2007-01"
-  "uk-2007-02"
-  "uk-union-2006-06-2007-05"
-  "gsh-2015"
-  "uk-2014"
-  "eu-2015"
+  "twitter-2010/twitter-2010"
+  "gsh-2015-host/gsh-2015-host"
+  "uk-2006-09/uk-2006-09"
+  "uk-2007-01/uk-2007-01"
+  "uk-2007-02/uk-2007-02"
+  "uk-union-2006-06-2007-05/uk-union-2006-06-2007-05-underlying"
+  "gsh-2015/gsh-2015"
+  "uk-2014/uk-2014"
+  "eu-2015/eu-2015"
 )
 BASE_URL="http://data.law.di.unimi.it/webdata"
 OUTPUT_FOLDER="../data/multi_gpu"
@@ -18,6 +18,7 @@ mkdir -p "$OUTPUT_FOLDER"
 for name in "${DATASETS[@]}"; do
   echo "---------------------------------"
   echo "Downloading $name..."
-  curl -f -C - -o "${OUTPUT_FOLDER}/${name}.properties" "${BASE_URL}/${name}/${name}.properties"
-  curl -f -C - -o "${OUTPUT_FOLDER}/${name}.graph" "${BASE_URL}/${name}/${name}.graph"
+  mkdir -p "${OUTPUT_FOLDER}/$(dirname "$name")"
+  curl -f -C - -o "${OUTPUT_FOLDER}/${name}.properties" "${BASE_URL}/${name}.properties"
+  curl -f -C - -o "${OUTPUT_FOLDER}/${name}.graph" "${BASE_URL}/${name}.graph"
 done
