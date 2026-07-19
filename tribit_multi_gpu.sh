5e5b6a9c3cb04c2bee4b9f34ee372c4cf39bd3cd
Sixte Oriol Llenas Segura <sixte99@gmail.com>
2026-07-19 17:09:15 +0200

    Played with the multigpu implementation

--- tribit_multi_gpu.sh
+++ tribit_multi_gpu.sh
@@ -7,5 +7,9 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
 
-srun singularity exec --nv tribit.sif /app/src/multi_gpu/target/release/rs artifact/sc26/data/multi_gpu/gsh-2015-host/gsh-2015-host 128 
+singularity exec --nv tribit.sif mpirun /app/src/multi_gpu/target/release/rs artifact/sc26/data/multi_gpu/gsh-2015-host/gsh-2015-host 128 
+exit
+mpirun src/multi_gpu/target/release/rs artifact/sc26/data/multi_gpu/gsh-2015-host/gsh-2015-host 128 
+exit
+srun --mpi=pmix singularity exec --nv tribit.sif /app/src/multi_gpu/target/release/rs artifact/sc26/data/multi_gpu/gsh-2015-host/gsh-2015-host 128 
 
