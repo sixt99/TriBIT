3fd04ce0798d77a40dfb978d6e2c997c2470cea5
Sixte Oriol Llenas Segura <sixte99@gmail.com>
2026-07-20 10:37:57 +0200

    Fixed tribit multi gpu script

--- tribit_multi_gpu.sh
+++ tribit_multi_gpu.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --account bsc03
 #SBATCH --qos=acc_debug
-#SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=80
 #SBATCH --time 01:00:00
 #SBATCH --ntasks-per-node=1
-#SBATCH --nodes=1
 
-singularity exec --nv tribit.sif mpirun /app/src/multi_gpu/target/release/rs artifact/sc26/data/multi_gpu/gsh-2015-host/gsh-2015-host 128 
-exit
-mpirun src/multi_gpu/target/release/rs artifact/sc26/data/multi_gpu/gsh-2015-host/gsh-2015-host 128 
-exit
-srun --mpi=pmix singularity exec --nv tribit.sif /app/src/multi_gpu/target/release/rs artifact/sc26/data/multi_gpu/gsh-2015-host/gsh-2015-host 128 
+DATA_PATH=artifact/sc26/data/multi_gpu/gsh-2015-host/gsh-2015-host 
+N_PARTITIONS=64
 
+# Run natively
+srun src/multi_gpu/target/release/rs $DATA_PATH $N_PARTITIONS 
+
+# Run under tribit.sif
+srun --mpi=pmix singularity exec --nv tribit.sif /app/src/multi_gpu/target/release/rs $DATA_PATH $N_PARTITIONS 
